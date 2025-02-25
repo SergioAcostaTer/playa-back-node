52931d7df3842f4ec2e9c84c58323cbf006f906f
sergioacostaquintana <sergioacostaquintana@gmail.com>
2025-02-25 18:17:13 +0000

    Eliminar archivos y configuraciones innecesarios

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-const mongoose = require('mongoose');
 const app = require('./app');
 const config = require('./config/config');
 const logger = require('./config/logger');
 
 let server;
-mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
-  logger.info('Connected to MongoDB');
+
+const startServer = () => {
   server = app.listen(config.port, () => {
     logger.info(`Listening to port ${config.port}`);
   });
-});
+};
 
-const exitHandler = () => {
+// Error handling for uncaught exceptions and unhandled promise rejections
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
   if (server) {
     server.close(() => {
       logger.info('Server closed');
@@ -22,17 +23,15 @@ const exitHandler = () => {
   }
 };
 
-const unexpectedErrorHandler = (error) => {
-  logger.error(error);
-  exitHandler();
-};
-
 process.on('uncaughtException', unexpectedErrorHandler);
 process.on('unhandledRejection', unexpectedErrorHandler);
 
+// handle SIGTERM (e.g., graceful shutdown on container termination)
 process.on('SIGTERM', () => {
   logger.info('SIGTERM received');
   if (server) {
     server.close();
   }
 });
+
+startServer();
