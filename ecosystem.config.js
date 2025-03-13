8a7e6879817bce95f039b44f1c1c0a035e2d36f1
Sergio Acosta Quintana <sergioacostaquintana@gmail.com>
2025-03-13 16:44:23 +0000

    Feat: add PORT configuration to ecosystem settings

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,8 @@ module.exports = {
         watch: false,
         max_memory_restart: '1G',
         env: {
-            NODE_ENV: 'production'
+            NODE_ENV: 'production',
+            PORT: 5150
         },
         env_file: '.env',
         // Add these new properties
