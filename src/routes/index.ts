830e116cf8746761f7940fe8ee4f0c27c53f0c35
sergio acosta <sergio acosta>
2025-03-24 19:07:47 +0000

    Feat: add reviews routes for fetching, creating, and updating reviews

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,13 @@ import { Router } from 'express'
 import { beaches } from './beaches'
 import { auth } from './auth'
 import { users } from './users'
+import { reviews } from './reviews'
 
 const router: Router = Router()
 
 const routes: {
   [key: string]: (router: Router) => void
-} = { beaches, auth, users }
+} = { beaches, auth, users, reviews }
 
 for (const route in routes) {
   routes[route](router)
--- src/routes/reviews.ts
+++ src/routes/reviews.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import { authGuard } from '@/guards'
 import { reviewsController } from '@/controllers/reviewsController'
 
-export const auth = (router: Router): void => {
+export const reviews = (router: Router): void => {
   router.get('/reviews/:beachId', reviewsController.getReviewsByBeach)
   router.post('/reviews', authGuard.isAuth, reviewsController.createReview)
   router.put('/reviews/:reviewId', authGuard.isAuth, reviewsController.updateReview)
