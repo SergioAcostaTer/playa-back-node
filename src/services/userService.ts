9522a7ee3fad68116653ae74d2a2eb1ac4d729d9
PedroBatistaS <pedrobatistasosa2002@gmail.com>
2025-04-28 19:25:02 +0100

    [add] user register

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,6 +1,7 @@
 import { IGoogleUser } from '@/contracts/user'
 import { db } from '@/dataSources'
 import { users } from '@/models/users'
+import bcrypt from 'bcrypt'
 
 export const userService = {
   async createUser(user: IGoogleUser) {
@@ -9,11 +10,40 @@ export const userService = {
       email: user.email,
       name: user.name,
       username: user.email.split('@')[0],
-      avatarUrl: user.picture
+      avatarUrl: user.picture,
+      passwordHash: null
     }
     const [userFromDb] = await db.insert(users).values(userToSave).returning()
     return userFromDb
   },
+
+  async createUserWithPassword({
+    email,
+    password,
+    name,
+    lastname
+  }: {
+    email: string
+    password: string
+    name: string, 
+    lastname: string
+  }) {
+    const passwordHash = await bcrypt.hash(password, 10)
+
+    const userToSave = {
+      email,
+      name,
+      lastname,
+      username: email.split('@')[0],
+      avatarUrl: null,
+      googleHash: null,
+      passwordHash
+    }
+
+    const [userFromDb] = await db.insert(users).values(userToSave).returning()
+    return userFromDb
+  },
+
   async getUserByGoogleId(googleHash: string) {
     return await db.query.users.findFirst({
       where: (user, { eq }) => eq(user.googleHash, googleHash)
